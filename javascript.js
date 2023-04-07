45d6f257eacacb24136c50b0bbb9256ebb48665f
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-04-07 16:40:39 +0530

    Add functionality to type and display numbers pressed

--- javascript.js
+++ javascript.js
@@ -1,26 +1,46 @@
-let firstInput = prompt('Write the First Number you want to add', '');
-let operator = prompt('Write the Operator', '');
-let lastInput = prompt('Write the Number', '');
-let firstNumber = parseInt(firstInput);
-let lastNumber = parseInt(lastInput);
+// let firstInput = prompt('Write the First Number you want to add', '');
+// let operator = prompt('Write the Operator', '');
+// let lastInput = prompt('Write the Number', '');
+// let firstNumber = parseInt(firstInput);
+// let lastNumber = parseInt(lastInput);
 
-function operate(operator,number1,number2){
-    if (number1) {
-        let value;
-        if (operator === '+') {
-            value = number1 + number2
-        }
-        else if (operator === '-') {
-            value = number1 - number2
-        }
-        else if (operator === '*') {
-            value = number1 * number2
-        }
-        else if (operator === '/') {
-            vaue = number1 / number2
-        }
-        console.log(value);
-    }
+let numbers = document.querySelectorAll('.numbers');
+let firstNumber = [];
+let lastNumber = [];
+let operator = '';
+
+for(i = 0; i < numbers.length;i++){
+    numbers[i].addEventListener('click',(e) => {
+        let displayText = document.createTextNode(e.target.id);
+        document.querySelector('.typeText').appendChild(displayText);
+        firstNumber.push(e.target.id);
+        console.log(firstNumber.join(''));
+    })
 }
 
-operate(operator,firstNumber,lastNumber);
+// function operate(operator,number1,number2){
+//     if (number1) {
+//         let value;
+//         if (operator === '+') {
+//             value = number1 + number2
+//         }
+//         else if (operator === '-') {
+//             value = number1 - number2
+//         }
+//         else if (operator === '*') {
+//             value = number1 * number2
+//         }
+//         else if (operator === '/') {
+//             vaue = number1 / number2
+//         }
+//         console.log(value);
+//     }
+// }
+
+// operate(operator,firstNumber,lastNumber);
+
+
+
+/** 1 2 3 4 5 6 7 8 9 0 .
+ *  0 1 2 3 4 5 6 7 8 9 10
+ */
