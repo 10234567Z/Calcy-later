2d9d9e1e84ba81c9738f3a05642e06b7ecf22621
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-04-08 15:31:49 +0530

    Complete display of firstnumber,operator and second number

--- javascript.js
+++ javascript.js
@@ -4,14 +4,24 @@ let typedText = document.querySelector('.typeText');
 let firstNumber = [];
 let lastNumber = [];
 let operator;
+let value;
 let operatorClicked = false;
 let percentageActive = false;
 
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', (e) => {
-        let displayText = document.createTextNode(e.target.id);
-        typedText.appendChild(displayText);
-        firstNumber.push(e.target.id);
+        if (operatorClicked === false) {
+            firstNumber.push(e.target.id);
+            typedText.innerHTML = `${firstNumber.join('')}`;
+            console.log(firstNumber.join(''));
+        }
+        else if(operatorClicked === true){
+            
+            lastNumber.push(e.target.id);
+            typedText.innerHTML = `${firstNumber.join('')} ${operator} ${lastNumber.join('')}`;
+            console.log(lastNumber.join(''));
+            
+        }
     })
 }
 
@@ -20,10 +30,10 @@ document.querySelector('.reset').addEventListener('click', (e) => {
     firstNumber.length = 0;
 })
 
-
 for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click', (e) => {
         operator = e.target.id;
-        typedText.innerHTML = firstNumber + operator;
+        typedText.innerHTML = `${firstNumber.join('')} ${operator} `;
+        operatorClicked = true;
     })
 }
