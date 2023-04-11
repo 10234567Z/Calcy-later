591e3e3b5bbffae2cf57ea96794bb8f99766205c
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-04-11 10:05:12 +0530

    Change font size upon click and fix other minor bugs

--- javascript.js
+++ javascript.js
@@ -1,34 +1,51 @@
+
 let numbers = document.querySelectorAll('.numbers');
 let operators = document.querySelectorAll('.operator');
+let buttons = document.querySelectorAll('.buttons');
 let typedText = document.querySelector('.typeText');
 let resultText = document.querySelector('.resultText');
+
 let firstInputs = [];
 let lastInputs = [];
-let operator;
 let value = 0;
-let operatorClicked = false;
-let percentageActive = false;
 let valueChanged = false;
 
+let operator;
+let operatorClicked = false;
+
 for (i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', (e) => {
+    numbers[i].addEventListener('mousedown', (e) => {
+        e.target.style.fontSize = '0.9em'
         if (operatorClicked === false && firstInputs.length <= 5) {
             firstInputs.push(e.target.id);
             typedText.innerHTML = `${firstInputs.join('')}`;
+            console.log(firstInputs);
         }
         else if (operatorClicked === true && lastInputs.length <= 5) {
 
             lastInputs.push(e.target.id);
             typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}`;
+            console.log(lastInputs);
             calculate();
         }
         else {
             alert('Numbers limit exceeded');
+            e.target.style.fontSize = '2em'
         }
     })
 }
 
-document.querySelector('.reset').addEventListener('click', (e) => {
+document.querySelector('.percentage').addEventListener('mousedown', (e) => {
+    if(operatorClicked === false){
+        typedText.innerHTML = `${firstInputs.join('')}` + e.target.id;
+    }
+    else{
+        typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}` + e.target.id;
+    }
+})
+
+document.querySelector('.reset').addEventListener('mousedown', (e) => {
+    e.target.style.fontSize = '0.8em'
     typedText.innerHTML = '';
     resultText.innerHTML = '';
     firstInputs.length = 0;
@@ -40,7 +57,8 @@ document.querySelector('.reset').addEventListener('click', (e) => {
 })
 
 for (i = 0; i < operators.length; i++) {
-    operators[i].addEventListener('click', (e) => {
+    operators[i].addEventListener('mousedown', (e) => {
+        e.target.style.fontSize = '0.8em'
         operator = e.target.id;
         typedText.innerHTML = `${firstInputs.join('')} ${operator} `;
         operatorClicked = true;
@@ -48,36 +66,49 @@ for (i = 0; i < operators.length; i++) {
         if (valueChanged === true) {
             lastInputs.length = 0;
             firstInputs.length = 0;
-            firstInputs.push(value);
+            firstInputs.push(value.toFixed(2));
             typedText.innerHTML = `${firstInputs.join('')} ${operator} `;
             resultText.innerHTML = '';
         }
-        if(operator === '='){
+        if (operator === '=') {
             typedText.innerHTML = `${firstInputs.join('')} `;
         }
     })
 }
 
-document.querySelector('.delete').addEventListener('click', (e) => {
-    if(operatorClicked === false){
+document.querySelector('.delete').addEventListener('mousedown', (e) => {
+
+    valueChanged = false;
+    if (operatorClicked === false) {
         firstInputs.length -= 1;
         typedText.innerHTML = `${firstInputs.join('')} `;
     }
-    else{
+    else if(lastInputs.length > 0) {
         lastInputs.length -= 1;
-        console.log(lastInputs);
         typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}`;
         calculate();
     }
+    else if(lastInputs.length === 0 || operatorClicked === true){
+        operatorClicked = false;
+        operator = '';
+        resultText.innerHTML = '';
+        value = '';
+        typedText.innerHTML = `${firstInputs.join('')}` ;
+    }
 })
 
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('mouseup', (e) => {
+        e.target.style.fontSize = '2em'
+    })
+}
 
 function calculate() {
 
     let firstNumber = parseFloat(firstInputs.join(''));
     let lastNumber = parseFloat(lastInputs.join(''));
 
-    if(valueChanged === false){
+    if (valueChanged === false) {
         if (operator === '+') {
             value = firstNumber + lastNumber;
             resultText.innerHTML = value;
@@ -98,7 +129,7 @@ function calculate() {
             resultText.innerHTML = value.toFixed(2);
             valueChanged = true;
         }
-        else if(operator === '='){
+        else if (operator === '=') {
             typedText.innerHTML = value.toFixed(2);
             resultText.innerHTML = '';
             valueChanged = true;
