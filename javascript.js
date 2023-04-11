282219308cfb5a142f746d357a83230fe27db709
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-04-11 12:58:03 +0530

    Add percentage functionality and fix other minor issues

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+/** Initialiozation of variables needed */
 let numbers = document.querySelectorAll('.numbers');
 let operators = document.querySelectorAll('.operator');
 let buttons = document.querySelectorAll('.buttons');
@@ -8,6 +8,7 @@ let resultText = document.querySelector('.resultText');
 let firstInputs = [];
 let lastInputs = [];
 let value = 0;
+let tempValue = 0;
 let valueChanged = false;
 
 let operator;
@@ -15,32 +16,52 @@ let operatorClicked = false;
 
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('mousedown', (e) => {
+
+        /** Change font size upon click */
         e.target.style.fontSize = '0.9em'
-        if (operatorClicked === false && firstInputs.length <= 5) {
+
+        /** Build the number before operator while keeping max length and other necessary checks */
+        if (operatorClicked === false && firstInputs.length <= 5 && firstInputs[firstInputs.length - 1] !== '%') {
             firstInputs.push(e.target.id);
             typedText.innerHTML = `${firstInputs.join('')}`;
-            console.log(firstInputs);
         }
-        else if (operatorClicked === true && lastInputs.length <= 5) {
+        /** Build the number after operator while keeping max length and other necessary checks */
+        else if (operatorClicked === true && lastInputs.length <= 5 && lastInputs[lastInputs.length - 1] !== '%') {
 
             lastInputs.push(e.target.id);
             typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}`;
-            console.log(lastInputs);
             calculate();
         }
+        /** If none of the above criteria meets , its invalid for calculator */
         else {
-            alert('Numbers limit exceeded');
+            alert('Invalid Input');
             e.target.style.fontSize = '2em'
         }
     })
 }
 
 document.querySelector('.percentage').addEventListener('mousedown', (e) => {
-    if(operatorClicked === false){
-        typedText.innerHTML = `${firstInputs.join('')}` + e.target.id;
+    let firstNumber = parseFloat(firstInputs.join(''));
+    let lastNumber = parseFloat(lastInputs.join(''));
+
+    /** Percentage for the 1st number and calculating it instantly */
+    if(operatorClicked === false && firstInputs.length > 0){
+        firstInputs.push('%');
+        typedText.innerHTML = `${firstInputs.join('')}`;
+        value = parseFloat((firstNumber / 100).toFixed(2));
+        resultText.innerHTML = value;
+        valueChanged = true;
+    }
+    else if(operatorClicked === true && lastInputs.length > 0){
+        lastInputs.push('%')
+        typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}`;
+        tempValue = parseFloat((lastNumber / 100).toFixed(2));
+        resultText.innerHTML = value;
+        valueChanged = true;
+        calculate();
     }
     else{
-        typedText.innerHTML = `${firstInputs.join('')} ${operator} ${lastInputs.join('')}` + e.target.id;
+        alert('Invalid Input');
     }
 })
 
@@ -79,7 +100,7 @@ for (i = 0; i < operators.length; i++) {
 document.querySelector('.delete').addEventListener('mousedown', (e) => {
 
     valueChanged = false;
-    if (operatorClicked === false) {
+    if (operatorClicked === false && firstInputs.length > 0) {
         firstInputs.length -= 1;
         typedText.innerHTML = `${firstInputs.join('')} `;
     }
@@ -94,6 +115,9 @@ document.querySelector('.delete').addEventListener('mousedown', (e) => {
         resultText.innerHTML = '';
         value = '';
         typedText.innerHTML = `${firstInputs.join('')}` ;
+    }
+    else{
+        alert('Invalid Input');
     }
 })
 
@@ -139,18 +163,34 @@ function calculate() {
         if (operator === '+') {
             value = firstNumber + lastNumber;
             resultText.innerHTML = value;
+            if(lastInputs.includes('%')){
+                value = firstNumber + tempValue;
+                resultText.innerHTML = value;
+            }
         }
         else if (operator === '-') {
             value = firstNumber - lastNumber;
             resultText.innerHTML = value;
+            if(lastInputs.includes('%')){
+                value = firstNumber - tempValue;
+                resultText.innerHTML = value;
+            }
         }
         else if (operator === 'x') {
             value = firstNumber * lastNumber;
             resultText.innerHTML = value.toFixed(2);
+            if(lastInputs.includes('%')){
+                value = firstNumber * tempValue;
+                resultText.innerHTML = value;
+            }
         }
         else if (operator === '÷') {
             value = firstNumber / lastNumber;
             resultText.innerHTML = value.toFixed(2);
+            if(lastInputs.includes('%')){
+                value = firstNumber / tempValue;
+                resultText.innerHTML = value;
+            }
         }
     }
 }
